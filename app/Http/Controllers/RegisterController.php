be6f283590580dff8fefb63133d4d4bd9c0d8257
johnathan221998 <thachson009@gmail.com>
2023-09-21 01:58:12 +0700

    push

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Htpp\Facades\S;
+use Illuminate\Support\Facades\Auth;
+use Hash;
 
 class RegisterController extends Controller
 {
-    
+    public function store(Request $resquest){
+        $input = $request->all();
+        User::create([
+            'firstname' => $input['firstname'],
+            'lastname' => $input['lastname'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password'])
+        ]);
+            return response()->json(['status'=> true,
+                                     'message'=> "Register Success"
+            ]);
+    }
 }
